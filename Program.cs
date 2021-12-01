ea18384695aa1bd84d5539a71f223741564cf48b
Andrzej Szymanski <aszymanski@MetaPack.local>
2021-12-01 23:57:51 +0100

    Add initial program with 3 classes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace Kompozycje
+namespace ConsoleApp29
 {
     class Program
     {
+        static void Display(Person person)
+        {
+            Console.WriteLine($"First name: {person.FirstName}");
+            Console.WriteLine($"Last name: {person.LastName}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Address address = new Address()
+            {
+                City = "Krakow",
+                PostalCode = "31-556",
+                Street = "Grodzka 5"
+            };
+
+            Person person = new Person()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Address = address
+            };
+
+            Display(person);
         }
     }
 }

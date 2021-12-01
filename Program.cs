60f9a9992e166b35a8d9e6a159cfd4893b69f08e
Andrzej Szymanski <aszymanski@MetaPack.local>
2021-12-01 23:48:55 +0100

    Główny program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kompozycje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
